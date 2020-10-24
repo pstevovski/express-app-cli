c6c7da9002e47eec96c69a9e0f367ac67313d1a3
pecko95 <stevovski.petar@gmail.com>
2020-10-24 16:05:24 +0200

    Updated devDependencies, added gitignore, added tsconfig.js and scripts

--- src/app.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-console.log("This is initial package publishing.")
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+#!usr/bin/env node
+
+function startApp(): void {
+    console.log("Starting application...");
+};
+
+startApp();
