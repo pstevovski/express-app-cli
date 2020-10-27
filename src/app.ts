450efa7d61b9b024147482db5feb087bbb1aa1d8
pecko95 <stevovski.petar@gmail.com>
2020-10-27 02:04:09 +0100

    Updated parseArguments with preset '--git' opttion and a template option to be received

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@
 import promptUser from "./commands/prompt";
 import { Answers } from "inquirer";
 import arg from "arg";
+import IParseArguments from "./interfaces/IParseArguments";
 
 async function startApp(): Promise<void> {
   console.log("Starting application...");
@@ -11,20 +12,28 @@ async function startApp(): Promise<void> {
 
   console.log("OPTIONS: ", options);
 
-  const answers: Answers = await promptUser();
+  const answers: Answers = await promptUser(options);
 
   console.log("Answers: ", answers);
 }
 
 // Parse the target directory argument
-function parseArguments() {
-  const args = arg({}, { argv: process.argv.slice(2) });
+function parseArguments(): IParseArguments {
+  const args = arg(
+    {
+      "--git": Boolean,
+      "-g": "--git",
+    },
+    { argv: process.argv.slice(2) },
+  );
 
   // If user types "." - default to current working directory
   if (args._[0] === ".") args._[0] = process.cwd();
 
   return {
-    directory: args._[0] || process.cwd(),
+    projectDirectory: args._[0] || process.cwd(),
+    git: args["--git"] || false,
+    template: args._[1],
   };
 }
 
