a6197940cfdde246dbb45f8c79a62c8bf10fe24f
pecko95 <stevovski.petar@gmail.com>
2020-10-27 01:46:15 +0100

    Parse the received target directory argument and return it as an option to be used

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,30 @@
 
 import promptUser from "./commands/prompt";
 import { Answers } from "inquirer";
+import arg from "arg";
 
 async function startApp(): Promise<void> {
   console.log("Starting application...");
 
+  const options = parseArguments();
+
+  console.log("OPTIONS: ", options);
+
   const answers: Answers = await promptUser();
 
   console.log("Answers: ", answers);
+}
+
+// Parse the target directory argument
+function parseArguments() {
+  const args = arg({}, { argv: process.argv.slice(2) });
+
+  // If user types "." - default to current working directory
+  if (args._[0] === ".") args._[0] = process.cwd();
+
+  return {
+    directory: args._[0] || process.cwd(),
+  };
 }
 
 startApp();
