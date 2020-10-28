1406d55649214d7137cb78c4fa2870a124f2040e
pecko95 <stevovski.petar@gmail.com>
2020-10-28 01:57:08 +0100

    Moved parseArguments to separate module and added options for formatting the path to the project directory

--- src/app.ts
+++ src/app.ts
@@ -2,39 +2,19 @@
 
 import promptUser from "./commands/prompt";
 import { Answers } from "inquirer";
-import arg from "arg";
+import parseArguments from "./parseArguments";
 import IParseArguments from "./interfaces/IParseArguments";
 
 async function startApp(): Promise<void> {
   console.log("Starting application...");
 
-  const options = parseArguments();
+  const options: IParseArguments = parseArguments();
 
   console.log("OPTIONS: ", options);
 
   const answers: Answers = await promptUser(options);
 
   console.log("Answers: ", answers);
-}
-
-// Parse the target directory argument
-function parseArguments(): IParseArguments {
-  const args = arg(
-    {
-      "--git": Boolean,
-      "-g": "--git",
-    },
-    { argv: process.argv.slice(2) },
-  );
-
-  // If user types "." - default to current working directory
-  if (args._[0] === ".") args._[0] = process.cwd();
-
-  return {
-    projectDirectory: args._[0] || process.cwd(),
-    git: args["--git"] || false,
-    template: args._[1],
-  };
 }
 
 startApp();
