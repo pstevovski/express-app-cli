993023be83545d20e88e888d32dfadd040f1fe2d
pecko95 <stevovski.petar@gmail.com>
2020-11-01 23:52:39 +0100

    Created default javascript template project structure. Added default dependencies to package.json. Added readme and entry points for api/routes.

---
+++ src/templates/javascript/src/api/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+module.exports = () => {
+  const app = express.Router();
+
+  // Import the route functions that you created in the ROUTES folder and call them, with **app** as argument.
+  
+  // Example:
+  // someTestRoute(app);
+
+  return app;
+}
