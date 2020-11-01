c76b1a47be432a9484ef572735c671a8972405e1
pecko95 <stevovski.petar@gmail.com>
2020-11-02 00:58:46 +0100

    Added loaders folder entry point, expressApp and a README file

---
+++ src/templates/javascript/src/loaders/index.js
@@ -0,0 +1,14 @@
+const expressApp = require("./express");
+
+async function loader(app) {
+    // Establish connection to the express server
+    await expressApp(app);
+    console.log("Express server started.")
+
+    // Establish connection to database
+
+    // More loaders can be added here...
+};
+
+module.exports = loader;
+
