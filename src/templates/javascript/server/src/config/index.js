17a1ad74dfa9b3a6c88939f1114d02bcf72cb616
pecko95 <stevovski.petar@gmail.com>
2020-11-14 17:47:12 +0100

    Removed .env and .gitignore from default files template. Removed config\index from js and ts templates

--- src/templates/javascript/server/src/config/index.js
+++
@@ -1,9 +0,0 @@
-// Handles the usage of .env files
-require("dotenv").config();
-
-// Get the environment in which the server is currently running
-const environment = (process.env.NODE_ENV || "development").toUpperCase();
-
-module.exports = {
-    PORT: process.env.PORT || 3000
-}
