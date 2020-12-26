fe95d6594ccf59299c41a1f3529af307fb844a2e
pecko95 <stevovski.petar@gmail.com>
2020-12-26 23:59:54 +0100

    Fixed undefined environemnt scenario and corrected the name for accessing the DB_USERNAME from the ENVs

--- src/templates/files/configs/mongodb/index.js
+++ src/templates/files/configs/mongodb/index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 
 // Get the environment in which the server is currently running
-const environment = process.env.NODE_ENV.toUpperCase();
+const environment = (process.env.NODE_ENV || "development").toUpperCase();
 
 module.exports = {
     PORT: process.env.PORT || 3000,
--- src/templates/files/configs/sql/index.ts
+++ src/templates/files/configs/sql/index.ts
@@ -20,7 +20,7 @@ const environment: string = (process.env.NODE_ENV ?? "development").toUpperCase(
 const config: ConfigSQL = {
     DB: process.env[`DB_${environment}`]!,
     PORT: process.env.PORT!,
-    DB_USER: process.env[`DB_USER_${environment}`]!,
+    DB_USER: process.env[`DB_USERNAME_${environment}`]!,
     DB_HOST: process.env[`DB_HOST_${environment}`]!,
     DB_PASSWORD: process.env[`DB_PASSWORD_${environment}`]!,
     DB_DIALECT: process.env.DB_DIALECT as DB_DIALECT
