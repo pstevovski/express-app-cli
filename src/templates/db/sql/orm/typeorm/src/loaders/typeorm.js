56c8872e358eac1104e16997e6b918758c3f4b49
pecko95 <stevovski.petar@gmail.com>
2020-12-26 23:57:10 +0100

    Added config import to handle the env values

--- src/templates/db/sql/orm/typeorm/src/loaders/typeorm.js
+++ src/templates/db/sql/orm/typeorm/src/loaders/typeorm.js
@@ -3,16 +3,17 @@ require("reflect-metadata");
 const typeorm = require("typeorm");
 const EntitySchema = typeorm.EntitySchema;
 const ExampleEntity = require("../db/entities/ExampleEntity");
+const config = require("../config/index");
 
 const db = async() => {
     try {
         await typeorm.createConnection({
-            type: DB_TYPE,
-            host: DB_HOST,
-            port: DB_PORT,
-            username: DB_USERNAME,
-            password: DB_PASSWORD,
-            database: DB,
+            type: config.DB_TYPE,
+            host: config.DB_HOST,
+            port: config.DB_PORT,
+            username: config.DB_USERNAME,
+            password: config.DB_PASSWORD,
+            database: config.DB,
 
             // Your entities here
             entities: [ new EntitySchema(ExampleEntity) ],
--- src/templates/db/sql/orm/typeorm/src/loaders/typeorm.ts
+++ src/templates/db/sql/orm/typeorm/src/loaders/typeorm.ts
@@ -1,16 +1,17 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { ExampleEntity } from "../db/entities/ExampleEntity";
+import config from "../config/index";
 
 const db = async(): Promise<void> => {
     try {
         await createConnection({
-            type: DB_TYPE,
-            host: DB_HOST,
-            port: DB_PORT,
-            username: DB_USERNAME,
-            password: DB_PASSWORD,
-            database: DB,
+            type: config.DB_TYPE,
+            host: config.DB_HOST,
+            port: config.DB_PORT,
+            username: config.DB_USERNAME,
+            password: config.DB_PASSWORD,
+            database: config.DB,
 
             // Your entities here
             entities: [ ExampleEntity ],
