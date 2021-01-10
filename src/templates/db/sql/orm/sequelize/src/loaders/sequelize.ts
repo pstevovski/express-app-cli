433fb898bbd2a5b36ce5e5db2174bcf22606f0f8
pecko95 <stevovski.petar@gmail.com>
2021-01-10 01:14:31 +0100

    Changed DB_DIALECT to DB_TYPE for more general use

--- src/templates/db/sql/orm/sequelize/src/loaders/sequelize.ts
+++ src/templates/db/sql/orm/sequelize/src/loaders/sequelize.ts
@@ -4,7 +4,7 @@ import config from "../config/index";
 // Create an instance of Sequelize
 const db = new Sequelize(config.DB, config.DB_USER, config.DB_PASSWORD, {
     host: config.DB_HOST,
-    dialect: config.DB_DIALECT,
+    dialect: config.DB_TYPE,
 
     // If using SQLite only - if not, remove
     // storage: 'path/to/database.sqlite'
