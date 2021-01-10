daef20db4b81ebb60089b77246708b3b30b9b158
pecko95 <stevovski.petar@gmail.com>
2021-01-10 01:16:32 +0100

    Added default values in the constructor

--- src/templates/db/sql/orm/typeorm/src/db/entities/ExampleEntity.ts
+++ src/templates/db/sql/orm/typeorm/src/db/entities/ExampleEntity.ts
@@ -2,6 +2,11 @@ import { Column, PrimaryGeneratedColumn, Entity } from "typeorm";
 
 @Entity()
 export class ExampleEntity {
+    constructor() {
+        this.id = 0;
+        this.name = "";
+    }
+    
     @PrimaryGeneratedColumn()
     id: number;
 
