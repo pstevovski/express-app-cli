9dfb095f7da28f2c48d3420801af051c7f32d29b
pecko95 <stevovski.petar@gmail.com>
2021-01-11 23:55:08 +0100

    Fixed tests for formatting the project path

--- tests/unit/project-path.test.ts
+++ tests/unit/project-path.test.ts
@@ -1,5 +1,6 @@
-import parseArguments from "../../src/parseArguments";
 import path from "path";
+
+import formatProjectPath from "../../src/commands/formatProjectPath";
 
 describe("PARSE ARGUMENTS:", () => {
     // Remove the pushed argument after each test
@@ -8,8 +9,8 @@ describe("PARSE ARGUMENTS:", () => {
     describe("Path to project directory:", () => {
         it("if user enters . as argument, it should get the path to the current working directory", () => {
             process.argv.push(".");
-            const { projectDirectory } = parseArguments(process.argv);
-            expect(projectDirectory).toMatch(process.cwd())
+            const directory = formatProjectPath(process.argv[2]);
+            expect(directory).toMatch(process.cwd())
         });
     
         it("should return false if provided path is not absolute", () => {
@@ -26,21 +27,21 @@ describe("PARSE ARGUMENTS:", () => {
     
         it("if user enters a relative path, it should concatenate it to the current working directory", () => {
             process.argv.push("testFolder\\");
-            const { projectDirectory } = parseArguments(process.argv);
+            const  directory  = formatProjectPath(process.argv[2]);
             const fullPath = path.join(process.cwd(), process.argv[2]);
-            expect(projectDirectory).toMatch(fullPath);
+            expect(directory).toMatch(fullPath);
         });
     
         it("should normalize and use the path to the project directory, if user specified an ABSOLUTE path", () => {
             process.argv.push("D:\\Learning\\");
-            const { projectDirectory } = parseArguments(process.argv);
+            const  directory  = formatProjectPath(process.argv[2]);
             const normalizedAbsolutePath = path.normalize(process.argv[2]);
-            expect(projectDirectory).toMatch(normalizedAbsolutePath);
+            expect(directory).toMatch(normalizedAbsolutePath);
         });
     
         it("should use the current working directory (.) if no path was provided as an argument", () => {
-            const { projectDirectory } = parseArguments(process.argv);
-            expect(projectDirectory).toMatch(process.cwd());
+            const  directory = formatProjectPath(process.argv[2]);
+            expect(directory).toMatch(process.cwd());
         });
     });
 });
