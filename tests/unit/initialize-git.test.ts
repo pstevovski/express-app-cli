2fbf8b091573be77d9f8354efe6c7a6dbdb6ad45
pecko95 <stevovski.petar@gmail.com>
2021-01-12 01:01:03 +0100

    Small update to git initialize test

--- tests/unit/initialize-git.test.ts
+++ tests/unit/initialize-git.test.ts
@@ -1,13 +1,22 @@
 import git from "../../src/commands/git";
 import formatProjectPath from "../../src/commands/formatProjectPath";
 import fs from "fs";
+import path from "path";
 
 describe("INITIALIZE GIT", () => {
+    // Remove every file from the test directory
+    const folderPath: string = "D:\\projects\\test\\";
+
+    afterEach(() => {
+        fs.rmdirSync(path.join(folderPath, ".git"), { recursive: true });
+        process.argv.pop();
+    });
+
     it ("should initialize a git repository in the selected project's directory", async() => {
         // Enter the directory's name
         process.argv.push("D:\\projects\\test\\");
 
-        const directory = formatProjectPath(process.argv[2]);
+        const directory = formatProjectPath(process.argv[process.argv.length - 1]);
 
         // Initialize git
         await git(directory);
