4ce93e2810ced2da96c7db7cfd221e40a986c3bc
pecko95 <stevovski.petar@gmail.com>
2021-01-12 01:09:05 +0100

    Updated jest config to include only commands and no templates from the src folder

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverageFrom: ["src/commands/*.ts"],
+    collectCoverage: true,
+    roots: ["<rootDir>/tests", "<rootDir>/src/commands"]
 };
